75f91ae638c5b648d6d5b4aee976275bc6f22cda
Dencho96 <denysvk.96@gmail.com>
2021-10-03 23:50:45 +0300

    added adaptive/ responsive, bugfix

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,34 @@
 const partnersSlider = new Swiper('.partners__slider', {
-    speed: 400,
-    spaceBetween: 100,
-    slidesPerView: 4,
-    loop: true,
-    navigation: {
-      nextEl: '.partners__btn--next',
-      prevEl: '.partners__btn--prev',
+  speed: 400,
+  spaceBetween: 100,
+  loop: true,
+  navigation: {
+    nextEl: '.partners__btn--next',
+    prevEl: '.partners__btn--prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1
     },
-  });
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1205: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+});
+
+$(".header__menu-btn").click(function () {
+  $(".header__menu-btn").toggleClass("is-active");
+});
+
+$('.header__menu-btn').on('click', function () {
+  $('.header__nav').slideToggle();
+});
